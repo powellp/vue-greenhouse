04a7810f72687b63143b11ad2ee828fe8b0dc075
prashant <prashant.powell@gmail.com>
2020-02-20 10:22:25 +0530

    First commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default{
+  apiKey: "AIzaSyDzRaMCFyEnf5pBkJpax1lnnMoeMFxX67I",
+  authDomain: "green-vue.firebaseapp.com",
+  databaseURL: "https://green-vue.firebaseio.com",
+  projectId: "green-vue",
+  storageBucket: "green-vue.appspot.com",
+  messagingSenderId: "1046143980227",
+  appId: "1:1046143980227:web:96542db374f243e4cb4876",
+  measurementId: "G-JPBPY4SYF1" 
+}
